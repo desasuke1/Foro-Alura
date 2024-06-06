d12c1740e43fadac5a39086aa224d42c69a15ab8
Dav <157630555+desasuke1@users.noreply.github.com>
2024-06-05 21:41:12 -0500

    Add files via upload

---
+++ ForoAlura/src/test/java/ChallengueApi/Foro/ForoApplicationTests.java
@@ -0,0 +1,13 @@
+package ChallengueApi.Foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
